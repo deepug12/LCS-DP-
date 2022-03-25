023c8b020b484e82856d1375a125685274bf30cf
Deepu Gupta <84670355+deepug12@users.noreply.github.com>
2022-03-25 12:20:58 +0530

    LCS _Topdown

---
+++ LCS_TopDown.cpp
@@ -0,0 +1,38 @@
+// Longest common subsequence
+// top - down technique
+#include<bits/stdc++.h>
+using namespace std;
+
+int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+        int dp[n+1][m+1];
+        for(int i = 0; i< n+1; i++){
+            for(int j = 0; j< m+1; j++){
+                if(i == 0 or j == 0){
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        for(int i = 1; i< n+1; i++){
+            for(int j = 1; j< m+1; j++){
+                if(text1[i-1] == text2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+        return dp[n][m];
+}
+int main(){
+	string s;
+	string p;
+	cin >> s;
+	cin >> p;
+
+
+	cout<<longestCommonSubsequence(s, p)<<'\n';
+
+}
